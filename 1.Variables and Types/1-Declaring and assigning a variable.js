e3a251cb7234efe9c8040192f9422a56c8b1a57f
chams <chamseddinechelly@gmail.com>
2015-10-09 22:25:04 +0100

    Declaring and assigning a variable

---
+++ 1.Variables and Types/1-Declaring and assigning a variable.js
@@ -0,0 +1,20 @@
+
+var x = 7;
+x;
+
+var myLocation = "La Marsa, Tunisia";
+myLocation;
+
+x = 3;
+x;
+
+myLocation = 31;
+myLocation;
+
+var animal1 = "cat", animal2 = "dog", animal3 = "monkey";
+animal1;
+animal2;
+animal3;
+
+// More info:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Lexical_grammar
