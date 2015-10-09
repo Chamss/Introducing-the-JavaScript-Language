bff67d289e5dd3f7a0afc2fbd98131c9f7a3fb07
chams <chamseddinechelly@gmail.com>
2015-10-09 22:55:19 +0100

    Strings

---
+++ 1.Variables and Types/3-Strings.js
@@ -0,0 +1,27 @@
+"This is a string"
+
+"11"
+
+'11'
+
+'This is also a string';
+'This is a string"; // mismatched quotes - this will not execute
+
+
+'<a href="">';
+'<a href="http://www.google.com">';
+
+"This is Chams's favorite string";
+"This is Chams's \"favorite\" string";
+"This is Chams's "favorite" string"; // this line will not work
+
+var myString = "this is mine";
+
+myString.length
+
+myString.toUpperCase();
+
+
+// More info:
+// https://developer.mozilla.org/en-US/docs/JavaScript/Guide/Values,_variables,_and_literals
+// https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/String
