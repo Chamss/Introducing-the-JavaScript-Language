b19e405caaed5c7b28fa0b4a4360e42aa887f342
chams <chamseddinechelly@gmail.com>
2015-10-09 22:38:48 +0100

    Numbers

---
+++ 1.Variables and Types/2-Numbers.js
@@ -0,0 +1,32 @@
+
+11
+
+11.00
+
+11.032
+
+Infinity
+
+
+-Infinity
+
+NaN
+
+Math;
+
+Math.round(1.05515484);
+1
+Math.round(1.95515484);
+2
+
+Math.random();
+
+
+
+
+// More info:
+// https://developer.mozilla.org/en-US/docs/JavaScript/Guide/Values,_variables,_and_literals
+// https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Infinity
+// https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Infinity
+// https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/NaN
+// https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Math
