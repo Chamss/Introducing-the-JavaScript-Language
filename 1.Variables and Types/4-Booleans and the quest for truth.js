4ef789efcb546673f3c1a5918da898a25ccadf71
chams <chamseddinechelly@gmail.com>
2015-10-09 23:03:59 +0100

    Booleans and the quest for truth

---
+++ 1.Variables and Types/4-Booleans and the quest for truth.js
@@ -0,0 +1,24 @@
+
+true
+
+false
+
+True
+Uncaught ReferenceError: True is not defined(…)
+
+var True = true;
+true
+
+var myLocation = "Tunis", myOtherLocation = "Sousse";
+undefined
+myLocation === myOtherLocation
+false
+myOtherLocation = "Tunis"
+"Tunis"
+myLocation === myOtherLocation
+true
+
+
+// More info:
+// https://developer.mozilla.org/en-US/docs/JavaScript/Guide/Values,_variables,_and_literals
+// https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Boolean
