13f6f1b00637df5abd137303b0cecd884f5ea3f3
Chams <chamseddinechelly@gmail.com>
2015-10-11 10:15:28 +0100

    Objects

---
+++ 2.Objects, Arrays, and More/1-Objects.js
@@ -0,0 +1,29 @@
+
+var emptyObject = {};
+undefined
+
+emptyObject;
+Object {}
+
+var notEmptyObject = {
+'label' : 'value',
+'label2' : 'value2',
+};
+undefined
+
+notEmptyObject;
+Object {label: "value", label2: "value2"}
+
+var person =   {
+    "_id": "561a27eda7cd9498a5c08692",
+    "isActive": false,
+    "age": 23,
+    "eyeColor": "green",
+    "name": "Anna Wilcox",
+    "gender": "female",
+    "email": "annawilcox@emtrac.com",
+    "phone": "+1 (979) 507-2092"
+  }
+
+  person
+Object {_id: "561a27eda7cd9498a5c08692", isActive: false, age: 23, eyeColor: "green", name: "Anna Wilcox"…}_id: "561a27eda7cd9498a5c08692"age: 23email: "annawilcox@emtrac.com"eyeColor: "green"gender: "female"isActive: falsename: "Anna Wilcox"phone: "+1 (979) 507-2092"__proto__: Object
